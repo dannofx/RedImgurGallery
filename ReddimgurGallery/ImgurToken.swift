3fc43d11fc7e2fc68a2c386eaac885e1e5ab560a
Daniel Heredia <danno@mistercyb.org>
2017-08-14 22:06:51 -0500

    Imgur token file added

---
+++ ReddimgurGallery/ImgurToken.swift
@@ -0,0 +1,14 @@
+//
+//  ImgurToken.swift
+//  RedImgurGallery
+//
+//  Created by Danno on 7/25/17.
+//  Copyright © 2017 Daniel Heredia. All rights reserved.
+//
+
+import Foundation
+
+struct ImgurCredentials {
+    static let clientID = "Client-ID XXXXXXXX" YOUR IMGUR TOKEN HERE
+    static let clientSecret = "XXXXXX"// this is not necessary by now
+}
